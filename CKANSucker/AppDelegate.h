2b1211a658896cfb4c033eac73823846b8ed6c68
Andrew Sage <andrew@asae.co.uk>
2014-10-02 17:32:04 +0100

    Initial Commit

---
+++ CKANSucker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CKANSucker
+//
+//  Created by Andrew Sage on 02/10/2014.
+//  Copyright (c) 2014 Xoverto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CKANSucker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CKANSucker
+//
+//  Created by Andrew Sage on 02/10/2014.
+//  Copyright (c) 2014 Xoverto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
